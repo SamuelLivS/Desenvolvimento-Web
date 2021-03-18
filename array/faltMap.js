8046262276378fc8f8ebf3f46d2b39b801f3e61b
Samuel de Sousa <samuellivramento122@gmail.com>
2021-03-18 10:05:23 -0300

    Iniciando o capítulo de node

--- array/faltMap.js
+++ array/faltMap.js
@@ -21,16 +21,15 @@ const escola = [{
 const getNotaDoAluno = aluno => aluno.nota
 const getNotasDaTurma = turma => turma.alunos.map(getNotaDoAluno)
 
+//FlatMap vai fazer isso aqui, de um jeito mais funcional
 const notas1 = escola.map(getNotasDaTurma)
 console.log(notas1)
-
-//FlatMap vai fazer isso aqui, de um jeito mais funcional
 console.log([].concat([ 8.1, 9.3 ], [ 8.9, 7.3 ]))
+
 
 //FlatMap.
 Array.prototype.flatMap = function(callback) {
     return Array.prototype.concat.apply([], this.map(callback))
 }
-
 const notas2 = escola.flatMap(getNotasDaTurma)
 console.log(notas2)
